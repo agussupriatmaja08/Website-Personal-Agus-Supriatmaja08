aab68a364a6749bcb0d83b50886f27af885a5196
agussupriatmaja08 <127099826+agussupriatmaja08@users.noreply.github.com>
2023-07-23 17:32:13 +0800

    Menambahkan file index.php

---
+++ web_profil.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agus Supriatmaja Personal Website</title>
+</head>
+
+<body>
+    <br>
+    <header style="text-align: center;">
+        <img src="img/aguss.jpg"  width="250" height="250" style="border-radius: 50%;"/>
+        <h1>Gede Agus Supriatmaja</h1>
+        <p>(Web Developer)</p>
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Hi, saya adalah web developer yang berdomisisli di Bali.
+            Saat ini sedang belajar Pemrograman Berbasis Website.
+        </p>
+    </article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Expert)</li>
+                            <li>CSS (Beginner)</li>
+                            <li>Javascript (Beginner)</li>
+                            <li>PHP (Beginner)</li>
+                            <li>Laravel (Beginner)</li>
+                            <li>NodeJs (Beginner)</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Panitia sie keamanan Suksesi HMJ TI</li>
+                            <li>Pernah membuat game sederhana menggunakan Greenfoot</li>
+                           
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2023 Agus Supriatmaja.</p>
+    </footer>
+</body>
+</html>
